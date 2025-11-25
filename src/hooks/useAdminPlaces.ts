f448443db2091bf8a33eb70d658e7774a06d92b6
JRam473 <juanramiro139@gmail.com>
2025-11-25 16:06:08 -0600

    add

--- src/hooks/useAdminPlaces.ts
+++ src/hooks/useAdminPlaces.ts
@@ -167,7 +167,7 @@ const handleError = (err: unknown): string | ModeracionError => {
         (errorData.error === 'CONTENIDO_RECHAZADO' || 
          errorData.error === 'IMAGEN_RECHAZADA' ||
          errorData.error === 'DESCRIPCION_RECHAZADA' ||
-         errorData.error === 'TEXTO_RECHAZADO' ||
+         errorData.error === 'TEXTO_RECHAZADOo' ||
          errorData.error === 'VALIDACION_RECHAZADA' ||
          errorData.error === 'PDF_RECHAZADO' ||
          errorData.error === 'PDF_INVALIDO')) {
