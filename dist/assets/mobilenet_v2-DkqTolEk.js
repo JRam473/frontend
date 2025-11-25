dd52629cbbfdc6204c3b6eeb2d30c678670a3439
JRam473 <juanramiro139@gmail.com>
2025-11-25 15:13:17 -0600

    add

---
+++ dist/assets/mobilenet_v2-DkqTolEk.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-FEn-eR-a.js";import"./animations-w5RCdeks.js";import"./vendor-BImi-76D.js";import"./charts-qq9cUDqM.js";import"./ui-vuw2n6JC.js";import"./utils-BNe0Xlio.js";var p=function(){return r(()=>import("./model.min-CdqdRUPQ.js"),[])},n=[function(){return r(()=>import("./group1-shard1of1.min-DUujGfpw.js"),[])}];export{p as modelJson,n as weightBundles};
